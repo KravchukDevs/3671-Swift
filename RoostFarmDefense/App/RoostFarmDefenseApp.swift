6935727ba9e7d3eb8a064d8172aaec5f336046fa
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-05 14:01:16 +0200

    init

---
+++ RoostFarmDefense/App/RoostFarmDefenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoostFarmDefenseApp.swift
+//  RoostFarmDefense
+//
+//  Created by Serhii Babchuk on 05.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoostFarmDefenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RFLaunchView()
+        }
+    }
+}
